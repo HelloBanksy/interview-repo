4332778958df771897bfbcecbe2e3dc8596ed6a1
Petr Friedrich <petr.friedrich@ataccama.com>
2024-01-31 11:44:19 +0100

    correct solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,24 @@ import { useState } from "react";
 import styled from "styled-components";
 import "./App.css";
 
-const items = [
+const movies = [
   {
-    title: "Test 1",
-    id: "text1",
+    title: "Inception",
+    id: "0f4b0978-9eb1-4d80-b2c2-2893f3b8f1e0",
     content:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+      "Inception is a mind-bending science fiction thriller directed by Christopher Nolan. The film explores the concept of dreams within dreams, taking the audience on a visually stunning and intellectually engaging journey through the layers of the subconscious.",
   },
   {
-    title: "Test 2",
-    id: "text2",
+    title: "The Shawshank Redemption",
+    id: "99a3c0e5-cf9b-4f11-8a3a-8eb24bc9a7b8",
     content:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+      "The Shawshank Redemption, directed by Frank Darabont, is a classic prison drama based on Stephen King's novella. The film follows the story of Andy Dufresne, a banker who is sentenced to life in Shawshank State Penitentiary, and his journey towards redemption and freedom.",
   },
   {
-    title: "Test 3",
-    id: "text3",
+    title: "Pulp Fiction",
+    id: "ceb1bc8d-9d6d-4ad0-9338-3947b84e1423",
     content:
-      "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.",
+      "Pulp Fiction, directed by Quentin Tarantino, is a non-linear narrative that weaves multiple interconnected stories. Known for its eclectic characters, witty dialogue, and unconventional structure, the film has become a cultural phenomenon and a defining work in contemporary cinema.",
   },
 ];
 
@@ -28,18 +28,18 @@ function App() {
 
   return (
     <Wrap>
-      {items.map((item) => {
+      {movies.map((movie) => {
         return (
-          <ItemWrap key={item.id}>
+          <ItemWrap key={movie.id}>
             <ItemTitle
               onClick={() =>
-                setActiveItem(activeItem === item.id ? undefined : item.id)
+                setActiveItem(activeItem === movie.id ? undefined : movie.id)
               }
             >
-              {item.title}
+              {movie.title}
             </ItemTitle>
-            {activeItem === item.id && (
-              <ItemContent>{item.content}</ItemContent>
+            {activeItem === movie.id && (
+              <ItemContent>{movie.content}</ItemContent>
             )}
           </ItemWrap>
         );
